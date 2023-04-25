5a850d400438231390db96c917e186ce049ad732
MariKotegova <m.kotegowa@yandex.ru>
2023-04-25 18:11:52 +0300

    init commit

---
+++ src/main/java/com/example/mysqljoin/MySqlJoinApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqljoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqlJoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySqlJoinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mysqljoin/MySqlJoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysqljoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySqlJoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
